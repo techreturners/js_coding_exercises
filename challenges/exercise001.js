1f6e72cbebde96c0beebce0c20a22872805b340b
Amali Fernando <amalifernando17@gmail.com>
2021-09-29 09:33:09 +0100

    four tasks in exercise ne

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -1,24 +1,57 @@
 function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
+  const lowerCaseWord = word.toLowerCase();
+  return lowerCaseWord.charAt(0).toUpperCase() + lowerCaseWord.slice(1);
 }
 
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
-  // Add your code here!
+  return firstName.charAt(0) +"."+ lastName.charAt(0);
 }
 
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
-  // Add your code here!
+  let priceWithVAT;
+  if(vatRate>0){
+    let vat = vatRate/100;
+    priceWithVAT = originalPrice + (originalPrice*vat);
+  }else{
+    priceWithVAT = originalPrice;
+  }
+  let price = (priceWithVAT - Math.floor(priceWithVAT)) !== 0;
+
+  if(price){
+    priceWithVAT = priceWithVAT.toFixed(2);
+  }
+  return parseFloat(priceWithVAT);
 }
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  // Add your code here!
+
+  let salePrice;
+
+  if(reduction>0){
+    let reductionPrice = reduction/100;
+    salePrice = originalPrice - (originalPrice*reductionPrice);
+  }else{
+    salePrice = originalPrice;
+  }
+
+  let price = (salePrice - Math.floor(salePrice)) !== 0;
+
+  if(price){
+    const salePriceStr = String(salePrice);
+    const decimalPlacesCount = salePriceStr.split('.')[1].length
+    if(decimalPlacesCount>2){
+      salePrice = salePrice.toFixed(2);
+    }
+  }
+
+  return parseFloat(salePrice);
 }
 
 function getMiddleCharacter(str) {
