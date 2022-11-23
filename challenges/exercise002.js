1bb822287a2b1954c6904ec3b55384d84c60b527
Ellie Judge <judge_ellie@hotmail.com>
2022-11-23 16:45:22 +0000

    updates exercises to use import / export

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,32 +1,24 @@
-function getFillings(sandwich) {
-	if (sandwich === undefined) throw new Error('ingredients is required');
-	// Your code here!
+export function getFillings(sandwich) {
+  if (sandwich === undefined) throw new Error("ingredients is required");
+  // Your code here!
 }
 
-function isFromManchester(person) {
-	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+export function isFromManchester(person) {
+  if (person === undefined) throw new Error("person is required");
+  // Your code here!
 }
 
-function getBusNumbers(people) {
-	if (people === undefined) throw new Error('people is required');
-	// Your code here!
+export function getBusNumbers(people) {
+  if (people === undefined) throw new Error("people is required");
+  // Your code here!
 }
 
-function countSheep(arr) {
-	if (arr === undefined) throw new Error('arr is required');
-	// Your code here!
+export function countSheep(arr) {
+  if (arr === undefined) throw new Error("arr is required");
+  // Your code here!
 }
 
-function hasMPostCode(person) {
-	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+export function hasMPostCode(person) {
+  if (person === undefined) throw new Error("person is required");
+  // Your code here!
 }
-
-module.exports = {
-	getFillings,
-	isFromManchester,
-	countSheep,
-	getBusNumbers,
-	hasMPostCode,
-};
--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,52 +1,41 @@
-function findSmallNums(nums) {
-	if (!nums) throw new Error('nums is required');
-	// Your code here
+export function findSmallNums(nums) {
+  if (!nums) throw new Error("nums is required");
+  // Your code here
 }
 
-function findNamesBeginningWith(names, char) {
-	if (!names) throw new Error('names is required');
-	if (!char) throw new Error('char is required');
-	// Your code here
+export function findNamesBeginningWith(names, char) {
+  if (!names) throw new Error("names is required");
+  if (!char) throw new Error("char is required");
+  // Your code here
 }
 
-function findVerbs(words) {
-	if (!words) throw new Error('words is required');
-	// Your code here
+export function findVerbs(words) {
+  if (!words) throw new Error("words is required");
+  // Your code here
 }
 
-function getIntegers(nums) {
-	if (!nums) throw new Error('nums is required');
-	// Your code here
+export function getIntegers(nums) {
+  if (!nums) throw new Error("nums is required");
+  // Your code here
 }
 
-function getCities(users) {
-	if (!users) throw new Error('users is required');
-	// Your code here
+export function getCities(users) {
+  if (!users) throw new Error("users is required");
+  // Your code here
 }
 
-function getSquareRoots(nums) {
-	if (!nums) throw new Error('nums is required');
-	// Your code here
+export function getSquareRoots(nums) {
+  if (!nums) throw new Error("nums is required");
+  // Your code here
 }
 
-function findSentencesContaining(sentences, str) {
-	if (!sentences) throw new Error('sentences is required');
-	if (!str) throw new Error('str is required');
-	// Your code here
+export function findSentencesContaining(sentences, str) {
+  if (!sentences) throw new Error("sentences is required");
+  if (!str) throw new Error("str is required");
+  // Your code here
 }
 
-function getLongestSides(triangles) {
-	if (!triangles) throw new Error('triangles is required');
-	// Your code here
+export function getLongestSides(triangles) {
+  if (!triangles) throw new Error("triangles is required");
+  // Your code here
 }
-
-module.exports = {
-	findSmallNums,
-	findNamesBeginningWith,
-	findVerbs,
-	getIntegers,
-	getCities,
-	getSquareRoots,
-	findSentencesContaining,
-	getLongestSides,
-};
