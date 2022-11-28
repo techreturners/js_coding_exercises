567fb2fe4c3bb163a51f7cfce19cb7cf8ede6d00
Tom <admin@Admins-MBP.lan>
2022-11-28 12:47:51 +0000

    Add captitalize function

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -2,7 +2,9 @@
 
 export function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
+  const capitalized = word.charAt(0).toUpperCase() + word.slice(1)
+
+  return capitalized;
 }
 
 export function generateInitials(firstName, lastName) {
