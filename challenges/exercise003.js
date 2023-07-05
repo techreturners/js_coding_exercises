0dbc72204fab4a2af9b47f74898b70e31a706e5b
unknown <marlon_tancredo@hotmail.com>
2023-07-05 12:31:54 +0100

    feat: Implementing getSquare function

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,26 +1,30 @@
 export function getSquares(nums) {
-	if (nums === undefined) throw new Error('nums is required');
-	// Your code here!
+  if (nums === undefined) throw new Error("nums is required");
+  // Your code here!
+  const squares = nums.map((num) => {
+    return num * num;
+  });
+  return squares;
 }
 
 export function camelCaseWords(words) {
-	if (words === undefined) throw new Error('words is required');
-	// Your code here!
+  if (words === undefined) throw new Error("words is required");
+  // Your code here!
 }
 
 export function getTotalSubjects(people) {
-	if (people === undefined) throw new Error('people is required');
-	// Your code here!
+  if (people === undefined) throw new Error("people is required");
+  // Your code here!
 }
 
 export function checkIngredients(menu, ingredient) {
-	if (menu === undefined) throw new Error('menu is required');
-	if (!ingredient) throw new Error('ingredient is required');
-	// Your code here!
+  if (menu === undefined) throw new Error("menu is required");
+  if (!ingredient) throw new Error("ingredient is required");
+  // Your code here!
 }
 
 export function duplicateNumbers(arr1, arr2) {
-	if (arr1 === undefined) throw new Error('arr1 is required');
-	if (arr2 === undefined) throw new Error('arr2 is required');
-	// Your code here!
+  if (arr1 === undefined) throw new Error("arr1 is required");
+  if (arr2 === undefined) throw new Error("arr2 is required");
+  // Your code here!
 }
