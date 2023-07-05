a1219594240421e5eaf8e34c4022027a88402216
unknown <marlon_tancredo@hotmail.com>
2023-07-05 12:07:43 +0100

    feat: Implementing getBuses function

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -15,6 +15,9 @@ export function isFromManchester(person) {
 export function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
   // Your code here!
+  const limit = 40;
+  let buses = Math.ceil(people / limit);
+  return buses;
 }
 
 export function countSheep(arr) {
