36c513cfa127cf22d8ef05903e7b39b1a8ddd386
unknown <marlon_tancredo@hotmail.com>
2023-07-05 11:41:09 +0100

    feat: implementing getFillings and isFromMAnchester function

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,24 +1,28 @@
 export function getFillings(sandwich) {
-	if (sandwich === undefined) throw new Error('ingredients is required');
-	// Your code here!
+  if (sandwich === undefined) throw new Error("ingredients is required");
+  // Your code here!
+  const { fillings } = sandwich;
+  return fillings;
 }
 
 export function isFromManchester(person) {
-	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+  if (person === undefined) throw new Error("person is required");
+  // Your code here!
+  const { city } = person;
+  return city === "Manchester" ? true : false;
 }
 
 export function getBusNumbers(people) {
-	if (people === undefined) throw new Error('people is required');
-	// Your code here!
+  if (people === undefined) throw new Error("people is required");
+  // Your code here!
 }
 
 export function countSheep(arr) {
-	if (arr === undefined) throw new Error('arr is required');
-	// Your code here!
+  if (arr === undefined) throw new Error("arr is required");
+  // Your code here!
 }
 
 export function hasMPostCode(person) {
-	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+  if (person === undefined) throw new Error("person is required");
+  // Your code here!
 }
