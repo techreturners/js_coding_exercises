e367c239a9b8dc33c069c02271a1fd143e9748b5
chadrakh <chadrak@outlook.com>
2023-10-06 11:28:12 +0100

    Add getSalePrice logic

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -28,7 +28,12 @@ export function getSalePrice(originalPrice, reduction) {
 	if (originalPrice === undefined)
 		throw new Error('originalPrice is required');
 	if (reduction === undefined) throw new Error('reduction is required');
-	// Add your code here!
+	
+	if(reduction == 0) {
+		return originalPrice;
+	}
+	
+	return originalPrice * (reduction / 100);
 }
 
 export function getMiddleCharacter(str) {
